ac5de9966a092dcd12a3fc5b18c498c74857a5b4
Sheharyar Ishfaq <sheharyarishfaq@gmail.com>
2024-06-04 09:02:25 +0500

    Updating earnings after 10 days using cron job

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ require("./config/db").connect();
 const express = require("express");
 const cors = require("cors");
 
+const cron = require("node-cron");
+
 // requiring the routes
 const userRoutes = require("./routes/user-routes");
 const propertyRoutes = require("./routes/property-routes");
@@ -14,6 +16,8 @@ const recentlyViewedRoutes = require("./routes/recently-viewed-routes");
 const bookingRoutes = require("./routes/booking-routes");
 const reviewRoutes = require("./routes/review-routes");
 const earningRoutes = require("./routes/earning-routes");
+
+const Earning = require("./models/earning-model");
 
 // getting the port from the environment variables
 const PORT = process.env.PORT || 5000;
@@ -37,6 +41,23 @@ app.use("/api/earnings", earningRoutes);
 // route to check if the api is running
 app.get("/", (req, res) => {
   res.send("Api is running...");
+});
+
+//cron job to update the earnings status to approved after 10 days
+cron.schedule("0 0 * * *", async () => {
+  const tenDaysAgo = new Date();
+  tenDaysAgo.setDate(tenDaysAgo.getDate() - 10);
+
+  try {
+    await Earning.updateMany(
+      { status: "pending", createdAt: { $lte: tenDaysAgo } },
+      { status: "approved" }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log("Earnings status updated to approved after 10 days");
 });
 
 // starting the server
