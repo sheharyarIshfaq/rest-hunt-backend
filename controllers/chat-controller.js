aae5a27235b47b86061ec20bbe729ecffce4b4d2
Sheharyar Ishfaq <sheharyarishfaq@gmail.com>
2024-06-04 11:13:27 +0500

    Checking senderid and receiver id when creating a chat

--- controllers/chat-controller.js
+++ controllers/chat-controller.js
@@ -1,7 +1,19 @@
 const Chat = require("../models/chat-model");
+const User = require("../models/user-model");
 
 const createChat = async (req, res) => {
   try {
+    //findt the sender and receiver chat
+
+    const senderId = await User.findById(req.body.senderId);
+    const receiver = await User.findById(req.body.receiverId);
+
+    if (!senderId || !receiver) {
+      return res.status(400).json({
+        message: "Sender or receiver not found",
+      });
+    }
+
     const newChat = new Chat({
       members: [req.body.senderId, req.body.receiverId],
     });
